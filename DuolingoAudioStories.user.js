00677fad1b28d12c45692cd0c7f276350c0be71e
Cal Stephens <cal@calstephens.tech>
2020-06-13 15:38:02 -0700

    Update CSS classes

--- DuolingoAudioStories.user.js
+++ DuolingoAudioStories.user.js
@@ -1,25 +1,41 @@
 // ==UserScript==
 // @name        Duolingo Audio Stories
 // @namespace   Violentmonkey Scripts
-// @match       *://stories.duolingo.com/*
+// @match       *://*.duolingo.com/stories/*
 // @grant       GM_addStyle
-// @version     1.0
+// @version     1.1
 // @author      Cal Stephens
 // @description Improve your listening comprehension by hiding the text in Duolingo stories
 // ==/UserScript==
+
+// Known CSS classes
+
+/// The outer-most CSS class of the Speech Bubbles
+const speech_bubble = "_3YBKO"
+
+/// Text that appears outside of speech bubbles
+const challenge_marker = "_2rXtz"
+const line_text_content = "_2rXtz"
+
+/// The text inside a speech bubble
+const synced_text = "JQRA4"
+const highlighted = "V0X_u"
+
+/// The "play audio" button
+const play_line_audio_button = "bV45r"
 
 
 // Hide the text in the speech bubble
 // unless the user hovers over it
 
 GM_addStyle(`
-  .speech-bubble:not(:hover) {
-    color: #def0a5;
-    background-color: #def0a5;
+  .${speech_bubble}:not(:hover) {
+    color: #def0a5 !important;
+    background-color: #def0a5 !important;
   }
 
-  .speech-bubble:hover {
-    color: #4c4c4c;
+  .${speech_bubble}:hover {
+    color: #4c4c4c !important;
   }
 `)
 
@@ -27,12 +43,12 @@ GM_addStyle(`
 // (exclude text in Challenge exercises)
 
 GM_addStyle(`
-  :not(.challenge-marker) > .line-text-content:not(:hover) {
+  :not(.${challenge_marker}) > .${line_text_content}:not(:hover) {
     color: #def0a5;
     background-color: #def0a5;
   }
 
-  :not(.challenge-marker) > .line-text-content:hover {
+  :not(.${challenge_marker}) > .${line_text_content}:hover {
     color: #4c4c4c;
   }
 `)
@@ -42,12 +58,12 @@ GM_addStyle(`
 // (synchronized with the hover state)
 
 GM_addStyle(`
-  .synced-text.highlighted {
-    color: inherit;
+  .${synced_text}.${highlighted} {
+    color: inherit
   }
 
-  .synced-text {
-   color: inherit;
+  .${synced_text} {
+    color: inherit
   }
 `);
 
@@ -56,7 +72,7 @@ GM_addStyle(`
 // with the rest of the bubble's hover state
 
 GM_addStyle(`
-  .speech-bubble::before {
+  .${speech_bubble}::before {
     border-bottom: 0px;
     border-right: 0px;
     border-top-left-radius: 0%;
@@ -66,7 +82,7 @@ GM_addStyle(`
     top: 0px;
   }
 
-  .speech-bubble::after {
+  .${speech_bubble}::after {
     border-bottom: 0px;
     border-right: 0px;
     content: '';
@@ -85,9 +101,12 @@ document.addEventListener('keyup', (e) => {
   if (e.code === "Space" && e.ctrlKey) {
     
     // Find the Play button...
-    var playButtons = document.getElementsByClassName("play-line-audio-button selected")
-    if (playButtons.count == 0) return
-    var playButton = playButtons[0]
+    var visiblePlayButtons = Array.from(
+        document.getElementsByClassName(play_line_audio_button))
+        .filter(button => button.offsetParent !== null);
+    
+    if (visiblePlayButtons.count == 0) return
+    var playButton = visiblePlayButtons[playButtons.length - 1]
     
     // and simulate a click event.
     playButton.click()
